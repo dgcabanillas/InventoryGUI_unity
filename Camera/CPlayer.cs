ce2a397182349604ce3e3d6f931c4a75b2210d91
Diego Cabanillas <dg.cabanillas@uni.pe>
2020-08-02 13:18:23 -0500

    camera controller

---
+++ Camera/CPlayer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject _Rueda;
+
+    private float _speed;
+    private float _rotateSpeed;
+    public Vector3 _direction;
+    private float _angleRotation;
+
+    private void Awake() {
+        _angleRotation = 40f;
+    }
+
+    public float Speed {
+        get { return _speed; }
+        set { _speed = value; }
+    }
+    public float RotateSpeed {
+        get { return _rotateSpeed; }
+        set { _rotateSpeed = value; }
+    }
+    public Vector3 Direction {
+        get { return _direction; }
+        set { _direction = value; }
+    }
+
+    public void MakeAction( STATE state ) {
+        switch( state ) {
+            case STATE.Move_along:
+                _direction = transform.position;
+                transform.Translate( Vector3.forward * _speed * Time.deltaTime );
+                _direction = Vector3.Normalize( transform.position - _direction );
+                // movimiento de la rueda
+                _Rueda.transform.Rotate( Vector3.forward * ( _speed * _angleRotation ) * Time.deltaTime );
+                break;
+            case STATE.Move_back:
+                _direction = transform.position;
+                transform.Translate( Vector3.forward * -_speed * Time.deltaTime );
+                _direction = Vector3.Normalize( _direction - transform.position );
+                // movimiento de la rueda
+                _Rueda.transform.Rotate( Vector3.forward * ( -_speed * _angleRotation ) * Time.deltaTime );
+                break;
+            case STATE.Rotate_left:
+                transform.Rotate( new Vector3(0, -_rotateSpeed, 0) * Time.deltaTime );
+                rotateDirection( _rotateSpeed * Time.deltaTime ); 
+                break;
+            case STATE.Rotate_right:
+                transform.Rotate( new Vector3(0, _rotateSpeed, 0) * Time.deltaTime );
+                rotateDirection( -_rotateSpeed * Time.deltaTime ); 
+                break;
+        }
+    } 
+
+    private void rotateDirection( float theta ) {
+        float _angle = theta * Mathf.PI / 180f;
+        float cosTheta = Mathf.Cos( _angle );
+        float sinTheta = Mathf.Sin( _angle );   
+        Vector3 newDirection = Vector3.zero;
+        newDirection.x = _direction.x * cosTheta - _direction.z * sinTheta;
+        newDirection.z = _direction.z * cosTheta + _direction.x * sinTheta;
+        _direction = newDirection;
+    }
+}
---
+++ Camera/GameController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public enum STATE {
+    Rotate_left,
+    Rotate_right,
+    Move_along,
+    Move_back,
+    Shoot
+}
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private GameObject playerObject;
+    [SerializeField] private GameObject shooterObject;
+    private CPlayer player;
+    private CShooter shooter;
+    private Vector3 posCamera;
+    private Vector3 rotCamera;
+    private float distance;
+    private float yPosition;
+
+    void Awake()
+    {
+        // Iniciamos datos de player
+        Assert.IsNotNull( playerObject );
+        playerObject.transform.position = Vector3.zero;
+        playerObject.transform.eulerAngles = Vector3.zero;
+        player = playerObject.GetComponent<CPlayer>();
+        player.Direction = Vector3.forward;
+        player.Speed = 10f;
+        player.RotateSpeed = 60f;
+
+        Assert.IsNotNull( shooterObject );
+        shooter = shooterObject.GetComponent<CShooter>();
+
+        // Iniciamos la cámara
+        distance = 8f;
+        yPosition = 6f;
+        transform.eulerAngles = new Vector3( 20f, 0, 0 );
+        PositionCamera();
+    }
+
+    void Update() {
+        if( Input.GetKey( KeyCode.W ) ) {
+            player.MakeAction( STATE.Move_along );
+            PositionCamera();
+        } else if( Input.GetKey( KeyCode.S ) ) {
+            player.MakeAction( STATE.Move_back );
+            PositionCamera();
+        } 
+        if( Input.GetKey( KeyCode.A ) ) {
+            player.MakeAction( STATE.Rotate_left );
+            PositionCamera();
+        } else if ( Input.GetKey( KeyCode.D ) ) {
+            player.MakeAction( STATE.Rotate_right );
+            PositionCamera();
+        }
+        if( Input.GetKey( KeyCode.Space ) ) {
+            shooter.MakeAction( STATE.Shoot );
+        }
+
+        if( Input.GetKey( KeyCode.UpArrow ) ) transform.Rotate( new Vector3( -0.5f, 0, 0) * 10f * Time.deltaTime );
+        if( Input.GetKey( KeyCode.DownArrow ) ) transform.Rotate( new Vector3( 0.5f, 0, 0) * 10f * Time.deltaTime );
+    }
+
+    private void PositionCamera() {
+        posCamera = playerObject.transform.position - ( player.Direction * distance );
+        posCamera.y = yPosition;
+        transform.position = posCamera;
+
+        rotCamera = playerObject.transform.eulerAngles;
+        rotCamera.x = transform.eulerAngles.x;
+        transform.eulerAngles = rotCamera;
+    }
+}
